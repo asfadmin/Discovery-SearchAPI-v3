4960b192ce85133b364ce883f43045dadc030601
Cameron Showalter <cjshowalter@alaska.edu>
2023-04-18 09:37:47 -0800

    Added spacing in the various dicts

--- SearchAPI/application/output.py
+++ SearchAPI/application/output.py
@@ -18,41 +18,41 @@ def as_output(search_generator: Generator[ASFSearchResults, None, None], output:
             }
         },
         'jsonlite2': {
-            'content':yield_jsonlite2(search_generator),
-            'media_type':'application/json; charset=utf-8',
-            'headers':{
+            'content': yield_jsonlite2(search_generator),
+            'media_type': 'application/json; charset=utf-8',
+            'headers': {
                 **constants.DEFAULT_HEADERS,
                 'Content-Disposition': f"attachment; filename={make_filename('json')}",
             }
         },
         'geojson': {
-            'content':yield_geojson(search_generator),
-            'media_type':'application/geojson; charset=utf-8',
-            'headers':{
+            'content': yield_geojson(search_generator),
+            'media_type': 'application/geojson; charset=utf-8',
+            'headers': {
                 **constants.DEFAULT_HEADERS,
                 'Content-Disposition': f"attachment; filename={make_filename('geojson')}",
             }
         },
         'csv': {
-            'content':yield_csv(search_generator),
-            'media_type':'text/csv; charset=utf-8',
-            'headers':{
+            'content': yield_csv(search_generator),
+            'media_type': 'text/csv; charset=utf-8',
+            'headers': {
                 **constants.DEFAULT_HEADERS,
                 'Content-Disposition': f"attachment; filename={make_filename('csv')}",
             }
         },
         'kml': {
-            'content':yield_kml(search_generator),
-            'media_type':'application/vnd.google-earth.kml+xml; charset=utf-8',
-            'headers':{
+            'content': yield_kml(search_generator),
+            'media_type': 'application/vnd.google-earth.kml+xml; charset=utf-8',
+            'headers': {
                 **constants.DEFAULT_HEADERS,
                 'Content-Disposition': f"attachment; filename={make_filename('kml')}",
             }
         },
         'metalink': {
-            'content':yield_metalink(search_generator),
-            'media_type':'application/metalink+xml; charset=utf-8',
-            'headers':{
+            'content': yield_metalink(search_generator),
+            'media_type': 'application/metalink+xml; charset=utf-8',
+            'headers': {
                 **constants.DEFAULT_HEADERS,
                 'Content-Disposition': f"attachment; filename={make_filename('metalink')}",
             }
