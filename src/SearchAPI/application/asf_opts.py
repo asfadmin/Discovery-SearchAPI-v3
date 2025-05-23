ab7ad4ca0c8d6fa1de5e18db2c6fc40a641e2f4c
SpicyGarlicAlbacoreRoll <ktfairbanks@alaska.edu>
2025-05-23 12:19:44 -0800

    bug: require searches have search related parameters

--- src/SearchAPI/application/asf_opts.py
+++ src/SearchAPI/application/asf_opts.py
@@ -13,6 +13,7 @@ from .asf_env import load_config_maturity
 
 from .logger import api_logger
 
+non_search_param = ['output', 'maxresults', 'pagesize', 'maturity']
 
 def string_to_range(v: Union[str, list]) -> tuple:
     if isinstance(v, list):
@@ -161,6 +162,16 @@ async def process_search_request(request: Request) -> SearchOptsModel:
     body_opts = get_asf_opts(body)
 
     query_opts.merge_args(**dict(body_opts))
+
+    try:
+        any_searchables = any([key.lower() not in non_search_param for key, _ in query_opts])
+        if not any_searchables:
+            raise ValueError(
+                'No searchable parameters specified, queries must include'
+                ' parameters besides output= and maxresults='
+            )
+    except ValueError as exc:
+        raise HTTPException(detail=repr(exc), status_code=400) from exc
 
     merged_args = {**query_params, **body}
 
