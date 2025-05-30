88f664b2f14a79c7a0061c6fc52aee21b250c255
SpicyGarlicAlbacoreRoll <ktfairbanks@alaska.edu>
2025-05-29 16:08:05 -0800

    fix: disable binary_media_types flag

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -71,7 +71,7 @@ class SearchAPIStack(Stack):
             id=api_id,
             handler=search_api_lambda,
             proxy=True,
-            binary_media_types=['multipart/form-data', 'application/octet-stream'],
+            # binary_media_types=['multipart/form-data', 'application/octet-stream'],
             default_cors_preflight_options=apigateway.CorsOptions(
                 allow_origins=apigateway.Cors.ALL_ORIGINS, allow_methods=apigateway.Cors.ALL_METHODS
             ),
