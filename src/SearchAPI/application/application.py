424217db697dd6a23f04b2eeb318ced599b0f643
SpicyGarlicAlbacoreRoll <ktfairbanks@alaska.edu>
2025-09-09 09:15:57 -0800

    cfg loaded as part of main module rather than at query time. Change nisar static layer url schema

--- src/SearchAPI/application/application.py
+++ src/SearchAPI/application/application.py
@@ -34,6 +34,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+cfg = load_config_maturity()
+cmr_health = get_cmr_health(cfg['cmr_base'], cfg['cmr_health'])
+
 
 @router.api_route("/services/search/param", methods=["GET", "POST", "HEAD"])
 async def query_params(searchOptions: SearchOptsModel = Depends(process_search_request)):
@@ -226,32 +229,30 @@ async def file_to_wkt(files: list[UploadFile]):
         headers=constants.DEFAULT_HEADERS
     )
 
-# @router.get('/redirect/{short_name}/{granule_id}')
-# async def nisar_static_layer(environment: Literal['prod', 'test'], short_name: str, granule_id: str):
-#         """
-#         environment: 'prod' or 'test' (whether to search the cmr prod or uat record)
-#         short_name: the CMR static layer collection short name to search
-#         granule_id: the granule id of the product to find the static layer for
+# example: https://api.daac.asf.alaska.edu/services/redirect/NISAR_L2_STATIC/{granule_id}.h5
+@router.get('/services/redirect/{short_name}/{granule_id}')
+async def nisar_static_layer(short_name: str, granule_id: str):
+        """
+        short_name: the CMR static layer collection short name to search
+        granule_id: the granule id of the product to find the static layer for
 
-#         returns: redirect to file url
-#         """
-#         opts = asf.ASFSearchOptions()
-#         if environment == 'test':
-#             opts.host = asf.INTERNAL.CMR_HOST_UAT
+        returns: redirect to file url
+        """
+        opts = asf.ASFSearchOptions(host=cfg['cmr_base'])
 
-#         try:
-#             granule = asf.search(
-#                 granule_list=[granule_id],
-#                 opts=opts
-#                 )[0]
-#         except IndexError:
-#             raise HTTPException(status_code=400, detail=f'Unable to find static layer, provided scene named "{granule_id}" not found in CMR record')
+        try:
+            granule = asf.search(
+                granule_list=[granule_id],
+                opts=opts
+                )[0]
+        except IndexError:
+            raise HTTPException(status_code=400, detail=f'Unable to find static layer, provided scene named "{granule_id}" not found in CMR record')
         
-#         static_layer = granule.get_static_layer(opts=asf.ASFSearchOptions(shortName=short_name))
-#         if static_layer is None:
-#             raise HTTPException(status_code=500, detail=f'Static layer not found for scene named "{granule_id}"')
+        static_layer = granule.get_static_layer(opts=asf.ASFSearchOptions(shortName=short_name))
+        if static_layer is None:
+            raise HTTPException(status_code=500, detail=f'Static layer not found for scene named "{granule_id}"')
 
-#         return RedirectResponse(static_layer.properties['url'])
+        return RedirectResponse(static_layer.properties['url'])
 
 
 def validate_wkt(wkt: str):
@@ -281,14 +282,11 @@ async def health_check():
         api_logger.info(exc)
         api_version = {'version': 'unknown'}
 
-    cfg = load_config_maturity()
-    cmr_health = get_cmr_health(cfg['cmr_base'], cfg['cmr_health'])
-
     api_health = {
         'ASFSearchAPI': {
             'ok?': True,
             'version': api_version['version'],
-            'config': load_config_maturity()
+            'config': cfg
         },
         'CMRSearchAPI': cmr_health
     }
