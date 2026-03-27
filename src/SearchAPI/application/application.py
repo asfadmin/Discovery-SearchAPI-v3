43a2f1f0163bd025b998f1332424f3947d29294e
SpicyGarlicAlbacoreRoll <ktfairbanks@alaska.edu>
2026-03-27 09:34:53 -0800

    chore: add exception handling to nisar_orbit_ephemera endpoint

--- src/SearchAPI/application/application.py
+++ src/SearchAPI/application/application.py
@@ -260,10 +260,16 @@ async def kml_to_footprint(granule: str, cmr_token: Optional[str] = None, maturi
 @router.get('/services/utils/nisar_orbit_ephemera')
 async def get_nisar_orbit_ephemera():
     """Returns the latest nisar orbit ephemera products for POE, MOE, NOE, and FOE in that order. Returns as jsonlite2"""
-    oe_dict = asf.utils.get_nisar_orbit_ephemeras()
-    results = ASFSearchResults([product for product in oe_dict.values()])
-    response_info = as_output(results, 'jsonlite2')
-    return Response(**response_info)
+    try:
+        oe_dict = asf.utils.get_nisar_orbit_ephemeras()
+        results = ASFSearchResults([product for product in oe_dict.values()])
+        response_info = as_output(results, 'jsonlite2')
+        return Response(**response_info)
+    except (asf.ASFSearchError, asf.CMRError, ValueError) as exc:
+        raise HTTPException(
+            detail=f"Search failed to find results: {exc}",
+            status_code=400
+        ) from exc
 
 # example: https://api.daac.asf.alaska.edu/services/redirect/NISAR_L2_STATIC/{granule_id}.h5
 # @router.get('/services/redirect/{short_name}/{granule_id}')
